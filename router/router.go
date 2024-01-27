17df6822ffb1b4e2ecc3a33d9740c7bf9e23cd78
aurorax-neo <15047150695@163.com>
2024-01-27 16:43:32 +0800

    修改根请求

--- router/router.go
+++ router/router.go
@@ -9,15 +9,16 @@ import (
 
 func SetRouter(router *gin.Engine) {
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-			"success": true,
-		})
-	})
+	router.GET("/", Index)
 	v1Router := router.Group("/v1")
 	v1Router.Use(middleware.V1Cors)
 	v1Router.Use(middleware.V1Response)
 	v1Router.Use(middleware.V1Auth)
 	v1Router.POST("/chat/completions", chat.Completions)
+}
+
+func Index(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello world! This is coze-chat-proxy.",
+	})
 }
